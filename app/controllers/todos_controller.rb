640ec366c390ed3198e14aaee37abdbffde41cab
vincentbwalker <vincethewalker@gmail.com>
2018-04-30 10:08:55 +0000

    Added Edit and Update function

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,15 +7,29 @@ class TodosController < ApplicationController
   def create
     @todo = Todo.new(todo_params)
     if @todo.save
-    flash[:notice] = "Todo was successfully created"
-    redirect_to todo_path(@todo)
+      flash[:notice] = "Todo was successfully created"
+      redirect_to todo_path(@todo)
     else
-    render 'new'
+      render 'new'
     end
   end
   
   def show
     @todo = Todo.find(params[:id])
+  end
+  
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+  
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      flash[:notice] = "Todo was successfully Updated"
+      redirect_to todo_path(@todo)
+    else
+      render 'edit'
+    end
   end
   
   private
