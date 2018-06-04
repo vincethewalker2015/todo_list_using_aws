52dbe8ffc1adf267ac45df66e3a9e221ae3725ee
vincentbwalker <vincethewalker@gmail.com>
2018-06-04 12:47:11 +0000

    resolved a bug in todos controller

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,4 @@
 class Todo < ApplicationRecord
-  validates :name, presence: true, length: {minimum: 5}
+  validates :name, presence: true, length: {minimum: 4}
   validates :description, presence: true, length: {maximum: 250}
 end
